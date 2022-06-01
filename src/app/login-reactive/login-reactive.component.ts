0d2eb0d7d79f9421eecf432eacb98475f2c1dc54
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-06-01 00:21:04 -0300

    FEAT: Change login to reactive example implementations

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "login",
@@ -7,6 +8,15 @@ import { Component, OnInit } from "@angular/core";
 })
 export class LoginReactiveComponent implements OnInit {
   constructor() {}
+
+  form = new FormGroup({
+    email: new FormControl("", {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl("", {
+      validators: [Validators.required, Validators.minLength(8)],
+    }),
+  });
 
   ngOnInit() {}
 }
