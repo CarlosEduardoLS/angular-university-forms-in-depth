85bed0fc697e2ac2dcef9aac6bff4d87519bbbce
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-06-01 22:59:39 -0300

    FEAT: Selection Dropdown Form Control

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FormBuilder, Validators } from "@angular/forms";
+import { Observable } from "rxjs";
 import { CoursesService } from "../../services/courses.service";
 import { courseTitleValidator } from "../../validators/course-title.validator";
+
+interface CourseCategory {
+  code: string;
+  description: string;
+}
 
 @Component({
   selector: "create-course-step-1",
   templateUrl: "./create-course-step-1.component.html",
   styleUrls: ["./create-course-step-1.component.scss"],
 })
-export class CreateCourseStep1Component {
+export class CreateCourseStep1Component implements OnInit {
   constructor(
     private readonly _fb: FormBuilder,
     private readonly courses: CoursesService
@@ -27,10 +33,17 @@ export class CreateCourseStep1Component {
         updateOn: "blur",
       },
     ],
+    category: ["BEGINNER", [Validators.required]],
     releasedAt: [new Date(), [Validators.required]],
     downloadsAllowed: [false, [Validators.requiredTrue]],
     longDescription: ["", [Validators.required, Validators.minLength(3)]],
   });
+
+  courseCategories$: Observable<CourseCategory[]>;
+
+  ngOnInit(): void {
+    this.courseCategories$ = this.courses.findCourseCategories();
+  }
 
   get courseTitle() {
     return this.form.controls["title"];
