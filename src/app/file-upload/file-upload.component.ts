0117b0368d12a4eddbfe2b3b7f93aa290c5933cd
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-07-05 00:59:39 -0300

    FEAT: Create a custom file upload component

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,8 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "file-upload",
   templateUrl: "file-upload.component.html",
   styleUrls: ["file-upload.component.scss"],
 })
-export class FileUploadComponent {}
+export class FileUploadComponent {
+  @Input() requiredFileType: string;
+
+  fileName: string = "";
+}
