cde3a82942f010e15ae4f0a126341cd83a220700
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-07-05 01:18:05 -0300

    FEAT: Uploading to a Node Server using the HTTP Client

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from "@angular/common/http";
 import { Component, Input } from "@angular/core";
+import { of } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 @Component({
   selector: "file-upload",
@@ -6,12 +9,30 @@ import { Component, Input } from "@angular/core";
   styleUrls: ["file-upload.component.scss"],
 })
 export class FileUploadComponent {
-  @Input() requiredFileType: string;
+  constructor(private readonly http: HttpClient) {}
 
   fileName: string = "";
+  fileUploadError = false;
+
+  @Input() requiredFileType: string;
 
   onFileSelected(event: any) {
     const file: File = event.target.files[0];
-    this.fileName = file.name;
+
+    if (file) {
+      this.fileName = file.name;
+      const formData = new FormData();
+      formData.append("thumbnail", file);
+      this.fileUploadError = false;
+      this.http
+        .post("/api/thumbnail-upload", formData)
+        .pipe(
+          catchError((error) => {
+            this.fileUploadError = true;
+            return of(error);
+          })
+        )
+        .subscribe();
+    }
   }
 }
