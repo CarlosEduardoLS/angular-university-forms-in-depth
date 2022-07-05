6b41a08af0d20d28dcc2433db16fe73e5194149d
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-07-05 03:01:30 -0300

    FEAT: Setting Up Step 3 of our Multi-step Form

--- src/app/create-course/create-course-step-3/create-course-step-3.component.ts
+++ src/app/create-course/create-course-step-3/create-course-step-3.component.ts
@@ -1,8 +1,13 @@
 import { Component } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "create-course-step-3",
   templateUrl: "create-course-step-3.component.html",
   styleUrls: ["create-course-step-3.component.scss"],
 })
-export class CreateCourseStep3Component {}
+export class CreateCourseStep3Component {
+  constructor(private readonly _fb: FormBuilder) {}
+
+  form = this._fb.group({});
+}
