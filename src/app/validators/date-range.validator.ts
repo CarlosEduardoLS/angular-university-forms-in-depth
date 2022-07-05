5d735b382ec6db1c26dc3e7d67d0061f2536e546
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-07-05 01:13:01 -0300

    FIX: Fix bug of dateTime is not a function

--- src/app/validators/date-range.validator.ts
+++ src/app/validators/date-range.validator.ts
@@ -2,9 +2,9 @@ import { FormGroup, ValidatorFn, Validators } from "@angular/forms";
 
 export function createPromoRangeValidator(): ValidatorFn {
   return (form: FormGroup): Validators | null => {
-    const start: Date = form.get("promoStartAt").value;
+    const start: Date = new Date(form.get("promoStartAt").value);
 
-    const end: Date = form.get("promoEndAt").value;
+    const end: Date = new Date(form.get("promoEndAt").value);
 
     if (start && end) {
       const isRangeValid = end.getTime() - start.getTime() > 0;
